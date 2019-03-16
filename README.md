ea763963d9210b106339084350ff7297a8f30c53
Amanda <38743110+amgava@users.noreply.github.com>
2019-03-16 03:59:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# BAMAZON
+
+### OVERVIEW
+Welcome to Bamazon.\
+This app simulates a storefront, using MySQL and Node.
+
+### HOW-TO
+Run node customerview.js in your terminal from the root folder of the repo.
---
+++ customerview.js
@@ -0,0 +1,89 @@
+//Required Modules
+const mysql = require('mysql');
+const Table = require('cli-table3');
+const inquirer = require('inquirer');
+
+//Initializing connection to database
+const dbconnect = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: 'rootroot',
+  database: 'bamazon'
+});
+
+//Connecting to database
+dbconnect.connect(function(err) {
+  if (err) {
+    console.error('error connecting to database');
+    return;
+  }
+
+  console.log('\n Database connected. Welcome to Bamazon. \n');
+});
+
+//Display database as table
+dbconnect.query('SELECT * FROM products', function(err, result) {
+  if (err) throw err;
+
+  var table = new Table({
+    head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
+  });
+  for (var i = 0; i < result.length; i++) {
+    table.push(
+      [result[i].item_id, result[i].product_name, result[i].department_name, '$'+ result[i].price, result[i].stock_quantity]);
+    }
+  console.log(table.toString() + '\n');
+  runSale();
+  }
+);
+
+//Defining function to prompt customer to make purchase selection, 
+//show price of selection, update database to reflect purchase
+function runSale(){
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'requested_item',
+      message: 'Please enter ID of the product you wish to purchase:'
+    },
+    {
+      type: 'input',
+      name: 'requested_units',
+      message: 'Please enter number of units you would like to purchase:'
+    }
+  ])
+  .then(answer => {
+    var item = answer.requested_item.toUpperCase();
+    var unit = answer.requested_units;
+    var iWant = 'SELECT * FROM products WHERE item_id = ?';
+
+    dbconnect.query(iWant, [item], function (err, result) {
+      if (err) throw err;
+
+      else {
+        if (result[0].stock_quantity < unit) {
+          console.log("Purchase cannot be completed. Insufficient stock.");
+          return;
+        }
+        else {
+          var orderTotal = result[0].price;
+          var stockUpdate = result[0].stock_quantity - unit;
+          var makePurchase = 'UPDATE products SET stock_quantity = ? WHERE item_id = ?';
+
+          dbconnect.query(makePurchase, [stockUpdate, item], function(err, response) {
+            if (err) throw err;
+
+            var output = [
+              response.changedRows +' order(s) received.',
+              'Your order total is $' + orderTotal,
+              'Thank you for shopping with Bamazon.'
+            ].join('\n');
+            console.log(output);
+          })
+        }
+      }
+    })
+  })
+}
